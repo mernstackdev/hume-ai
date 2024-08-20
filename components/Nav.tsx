5ef6221b4a9fcbae96373d5b8e2c38dde1ef361b
mernstackdev <95236570+mernstackdev@users.noreply.github.com>
2024-08-21 02:18:59 +0500

    update logo

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,31 +1,8 @@
 "use client";
 
-import { useLayoutEffect, useState } from "react";
-import HumeLogo from "./logos/Hume";
-import { Button } from "./ui/button";
-import { Moon, Sun } from "lucide-react";
-import Github from "./logos/GitHub";
-import pkg from '@/package.json';
+import Image from "next/image";
 
 export const Nav = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  useLayoutEffect(() => {
-    const el = document.documentElement;
-
-    if (el.classList.contains("dark")) {
-      setIsDarkMode(true);
-    } else {
-      setIsDarkMode(false);
-    }
-  }, []);
-
-  const toggleDark = () => {
-    const el = document.documentElement;
-    el.classList.toggle("dark");
-    setIsDarkMode((prev) => !prev);
-  };
-
   return (
     <div
       className={
@@ -33,39 +10,7 @@ export const Nav = () => {
       }
     >
       <div>
-        <HumeLogo className={"h-5 w-auto"} />
-      </div>
-      <div className={"ml-auto flex items-center gap-1"}>
-        <Button
-          onClick={() => {
-            window.open(
-              pkg.homepage,
-              "_blank",
-              "noopener noreferrer"
-            );
-          }}
-          variant={"ghost"}
-          className={"ml-auto flex items-center gap-1.5"}
-        >
-          <span>
-            <Github className={"size-4"} />
-          </span>
-          <span>Star on GitHub</span>
-        </Button>
-        <Button
-          onClick={toggleDark}
-          variant={"ghost"}
-          className={"ml-auto flex items-center gap-1.5"}
-        >
-          <span>
-            {isDarkMode ? (
-              <Sun className={"size-4"} />
-            ) : (
-              <Moon className={"size-4"} />
-            )}
-          </span>
-          <span>{isDarkMode ? "Light" : "Dark"} Mode</span>
-        </Button>
+        <Image src="/logo.svg" alt="Voicing AI" width="100" height="100" />
       </div>
     </div>
   );
